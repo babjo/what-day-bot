9d21eb694cd0819b21d0ad38a98a2b995a82359b
babjo <kd980311@naver.com>
2018-02-11 16:02:06 +0900

    Fix Room bug

--- src/main/java/com/babjo/whatdaybot/Room.java
+++ src/main/java/com/babjo/whatdaybot/Room.java
@@ -3,17 +3,33 @@ package com.babjo.whatdaybot;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import lombok.Data;
-
 @Entity
-@Data
 public class Room {
     @Id
     private String id;
     private boolean botRunning;
 
+    public Room() {
+    }
+
     public Room(String id, boolean botRunning) {
         this.id = id;
+        this.botRunning = botRunning;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isBotRunning() {
+        return botRunning;
+    }
+
+    public void setBotRunning(boolean botRunning) {
         this.botRunning = botRunning;
     }
 }
