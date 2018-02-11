6bf164867828bbdecfaf3872285def627bec80a0
babjo <kd980311@naver.com>
2018-02-11 17:09:33 +0900

    Add EnableScheduling annotation

--- src/main/java/com/babjo/whatdaybot/Config.java
+++ src/main/java/com/babjo/whatdaybot/Config.java
@@ -10,8 +10,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 public class Config {
     @Bean
     @Primary
