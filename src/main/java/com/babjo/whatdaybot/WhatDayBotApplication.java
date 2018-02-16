fb802ca50d50ff533bc917ad297068596136d8bc
babjo <kd980311@naver.com>
2018-02-17 01:41:02 +0900

    Add root to check health

--- src/main/java/com/babjo/whatdaybot/WhatDayBotApplication.java
+++ src/main/java/com/babjo/whatdaybot/WhatDayBotApplication.java
@@ -2,10 +2,18 @@ package com.babjo.whatdaybot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WhatDayBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(WhatDayBotApplication.class, args);
+    }
+
+    @GetMapping(value = "/health")
+    public String checkHealthy() {
+        return "OK";
     }
 }
