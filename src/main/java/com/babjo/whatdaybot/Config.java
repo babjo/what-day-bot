c48477241b4ec9c2f8c629c331693c3d3ac87905
babjo <kd980311@naver.com>
2018-04-08 15:26:42 +0900

    Migrate it to spring-boot2

--- src/main/java/com/babjo/whatdaybot/Config.java
+++ src/main/java/com/babjo/whatdaybot/Config.java
@@ -9,9 +9,8 @@ import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
