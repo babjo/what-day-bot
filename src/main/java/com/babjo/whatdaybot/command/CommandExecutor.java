d5a72abc1176ba6a0e90853b386ae56b41edd99b
babjo <kd980311@naver.com>
2018-07-15 10:33:24 +0900

    Test

--- src/main/java/com/babjo/whatdaybot/command/CommandExecutor.java
+++ src/main/java/com/babjo/whatdaybot/command/CommandExecutor.java
@@ -2,6 +2,10 @@ package com.babjo.whatdaybot.command;
 
 import java.time.Clock;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.babjo.naver.client.PeriodicRisingKeywordCrawler;
 import com.babjo.whatdaybot.repository.RoomRepository;
@@ -10,10 +14,12 @@ import com.google.common.collect.ImmutableList;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.MessageContent;
 import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.VideoMessage;
 
 public class CommandExecutor {
 
     private final List<Command> commands;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandExecutor.class);
 
     public CommandExecutor(RoomRepository roomRepository,
                            PeriodicRisingKeywordCrawler periodicRisingKeywordCrawler,
@@ -28,7 +34,20 @@ public class CommandExecutor {
                                     new MondaySongCommand(),
                                     new RisingKeywordsCommand(periodicRisingKeywordCrawler),
                                     new AllRoomStateCommand(roomRepository),
-                                    new SalaryCommand(clock));
+                                    new SalaryCommand(clock),
+        new Command() {
+            @Override
+            public Pattern getPattern() {
+                LOGGER.info("getPattern");
+                return Pattern.compile("아니");
+            }
+
+            @Override
+            public Message execute(MessageEvent<MessageContent> event) {
+                LOGGER.info("Image");
+                return new VideoMessage("https://media.giphy.com/media/d3FyFzuFXikYruxy/giphy.gif", "https://media.giphy.com/media/d3FyFzuFXikYruxy/giphy.gif");
+            }
+        });
     }
 
     public Message execute(MessageEvent<MessageContent> event) {
